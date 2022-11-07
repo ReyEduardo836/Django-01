ae2428ffcc584f3e7a88c603d288a59385872b37
ReyEduardo836 <reyeduardohalsallquintero8@gmail.com>
2022-11-06 22:19:56 -0600

    Creando el template de Detail y haciendo funcionar el metodo de Vote para votar una respuesta y se mire reflejado en la base de datos

--- premiosplatziapp/polls/views.py
+++ premiosplatziapp/polls/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 
-from .models import Question
+from .models import Question, Choice
 
 # Create your views here.
 
@@ -26,5 +27,17 @@ def detail(request, question_id):
 def results(request, question_id):
     return HttpResponse(f"Estas viendo los resultados de la pregunta numero {question_id}")
 
+
 def vote(request, question_id):
-    return HttpResponse(f"Estas votando a la pregunta numero {question_id}")
+    question = get_object_or_404(Question, pk=question_id)
+    try: 
+        selected_choice = question.choice_set.get(pk=request.POST["choice"])
+    except(KeyError, Choice.DoesNotExist):
+        return render(request, "polls/detail.html", {
+            "question": question,
+            "error_message": "No elegiste una respuesta",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
