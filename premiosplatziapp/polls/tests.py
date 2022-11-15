ee3affa9e09c2e0906ab39e22635b3cd4400b79e
ReyEduardo836 <reyeduardohalsallquintero8@gmail.com>
2022-11-15 13:13:23 -0600

    Solucionando el error encontrado por el test y creando nuevos test para el pasado y presente

--- premiosplatziapp/polls/tests.py
+++ premiosplatziapp/polls/tests.py
@@ -22,3 +22,21 @@ class QuestionModelTests(TestCase):
         time = timezone.now() + datetime.timedelta(days=30)
         future_question = Question(question_text = "Quien es el mejor Course Director de platzi",pub_date = time)
         self.assertIs(future_question.was_published_recently(), False)
+
+    def test_was_published_resently_with_past_question(self):
+        """
+        was_published_recently retorna False para las preguntas
+        en las cuales pub_date estan en el pasado
+        """
+        time = timezone.now() - datetime.timedelta(days=30)
+        past_question = Question(question_text = "Quien es el mejor Course Director de platzi", pub_date = time)
+        self.assertIs(past_question.was_published_recently(), False)
+
+    def test_was_published_resently_with_present_question(self):
+        """
+        was_published_recently retorna True para las preguntas
+        en las cuales pub_date estan en el presente
+        """
+        time = timezone.now()
+        present_question = Question(question_text = "Quien es el mejor Course Director de platzi", pub_date = time)
+        self.assertIs(present_question.was_published_recently(), True)
