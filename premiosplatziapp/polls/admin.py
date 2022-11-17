9a1dd371181be9cbc51f197450be178657ecad2a
ReyEduardo836 <reyeduardohalsallquintero8@gmail.com>
2022-11-17 11:47:02 -0600

    Mejorando el Admin de Django - colocandole que se puedan agregar respuestas a las preguntas desde el administrador de django

--- premiosplatziapp/polls/admin.py
+++ premiosplatziapp/polls/admin.py
@@ -2,6 +2,15 @@ from django.contrib import admin
 
 from .models import Question, Choice
 
+class ChoiceInLine(admin.StackedInline):
+    model = Choice
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    fields = ["pub_date", "question_text"]
+    inlines = [ChoiceInLine]
+
+
 # Register your models here.
-admin.site.register(Question)
+admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
