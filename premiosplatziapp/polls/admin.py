0eac57be6604ba996b164d0febee3c8ca93e3a40
ReyEduardo836 <reyeduardohalsallquintero8@gmail.com>
2022-11-17 11:57:14 -0600

    Mejorando el Admin de Django - Agregando mas informacion donde se visualizan las preguntas, Agregando filtrado por fecha, Agregando una busqueda por el texto de la pregunta

--- premiosplatziapp/polls/admin.py
+++ premiosplatziapp/polls/admin.py
@@ -9,8 +9,11 @@ class ChoiceInLine(admin.StackedInline):
 class QuestionAdmin(admin.ModelAdmin):
     fields = ["pub_date", "question_text"]
     inlines = [ChoiceInLine]
+    list_display = ("question_text", "pub_date", "was_published_recently")
+    list_filter = ["pub_date"]
+    search_fields = ["question_text"]
 
 
 # Register your models here.
 admin.site.register(Question, QuestionAdmin)
-admin.site.register(Choice)
+# admin.site.register(Choice)
